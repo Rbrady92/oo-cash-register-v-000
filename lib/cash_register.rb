fa21ddd9aee7ddca992a10f061c0654e2ee591e8
Ryan Brady <ryanbrady369@gmail.com>
2018-10-30 21:56:20 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,7 +5,7 @@ class CashRegister
     @last_item = 0
     @total = 0
     @item_list = []
-    @emp_disc = emp_disc / 100.00
+    @emp_disc = emp_disc            #or let me do / 100.00 on initialization
   end
 
   def total
@@ -26,7 +26,8 @@ class CashRegister
   end
 
   def apply_discount
-    @total -= (total * @emp_disc)
+    emp_disc = @empl
+    @total -= (total * (@emp_disc.to_f / 100.00))
     @total == 0 ? "There is no discount to apply." : "After the discount, the total comes to $#{@total.floor}."
   end
 
