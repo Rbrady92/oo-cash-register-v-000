d7d2f896582d50a0598be61158b65eb275d61d89
Ryan Brady <ryanbrady369@gmail.com>
2018-10-30 21:24:11 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,34 +1,34 @@
 class CashRegister
-  attr_accessor 
+  attr_accessor
 
   def initialize(emp_disc = .2)
     @last_item = 0
     @total = 0
     @item_list = []
     @emp_disc = emp_disc
-  end 
+  end
 
   def total
     @total
-  end 
+  end
 
   def add_item(item, price, quantity = 1)
     item_total = (price * quantity).round(2)
     @total += item_total
     @last_item = item_total
     @item_list << item
-  end 
+  end
 
   def items
     @item_list
-  end 
+  end
 
-  def apply_discount 
-    
-  end 
-  
+  def apply_discount
+
+  end
+
   def void_last_transaction
     @total -= @last_item
-  end 
+  end
 
 end 
